3a5cbf57fe5a281b4175c5ea7b66d9f6fa7381c3
Jan Schering <jan.schering@blueonic.com>
2021-02-18 09:19:25 +0100

    ScalaJS Basics: DOM Manipulation, DOM Simulation using JSDOM, Unit Testing

--- build.sbt
+++ build.sbt
@@ -7,3 +7,11 @@ scalaVersion := "2.13.1" // or any other Scala version >= 2.11.12
 scalaJSUseMainModuleInitializer := true
 
 libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "1.1.0"
+
+//Simulate DOM in NodeJS environment
+//NOTE: Alternatively to JSDOM we could use Selenium
+jsEnv := new org.scalajs.jsenv.jsdomnodejs.JSDOMNodeJSEnv()
+
+// Adding the uTest Framework as a dependency
+libraryDependencies += "com.lihaoyi" %%% "utest" % "0.7.4" % "test"
+testFrameworks += new TestFramework("utest.runner.Framework")
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,6 +2,7 @@ package tutorial.webapp
 
 import org.scalajs.dom
 import org.scalajs.dom.document
+import scala.scalajs.js.annotation.JSExportTopLevel
 
 object TutorialApp {
   def main(args: Array[String]): Unit = {
@@ -10,11 +11,44 @@ object TutorialApp {
 
     // We can use the Function we wrote to append a TextNode to an existing DomNode
     appendPar(document.body, "Hello World")
+
+    // Instead of accessing exisiting Nodes in the DOM, we can create and insert our own
+    val button = document.createElement("button")
+    button.textContent = "This Button is generated from the ScalaJS script"
+    button.addEventListener("click", { (e: dom.MouseEvent) =>
+      addClickedMessage()
+    })
+    document.body.appendChild(button)
+
+    // Instead of setting up the UI asynchronously in the main, we can wait for the DOM to load and then render it:
+    document.addEventListener("DOMContentLoaded", { (e: dom.Event) =>
+      setupUI()
+    })
   }
 
+  def setupUI(): Unit = {
+    val button = document.createElement("button")
+    button.textContent = "Click me!"
+    button.addEventListener("click", { (e: dom.MouseEvent) =>
+      addClickedMessage()
+    })
+    document.body.appendChild(button)
+
+    appendPar(document.body, "Hello World")
+  }
+
+  // A function that allows us to append a TextNode to an existing DOM NODE
   def appendPar(targetNode: dom.Node, text: String): Unit = {
     val parNode = document.createElement("p")
     parNode.textContent = text
     targetNode.appendChild(parNode)
   }
+
+  // The JSExportTopLevel annotation allows to create a Top-Level Function in Javascript
+  @JSExportTopLevel("addClickedMessage")
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
+  }
+
+
 }
